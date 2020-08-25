60763f0e2123ed14f3734b7f47fd5b697beab784
Tom Nguyen <tommy.nguyen0721@gmail.com>
2020-08-24 21:00:54 -0700

    Added more CSS

--- client/src/components/morePlaces.jsx
+++ client/src/components/morePlaces.jsx
@@ -68,6 +68,7 @@ class MorePlaces extends React.Component {
   }
 }
 
+
 const Header = styled.h2`
   font-weight: 400;
   font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif !important;
@@ -83,25 +84,45 @@ const NavBar = styled.div`
 `;
 
 const PrevButton = styled.button`
-  border: 1px solid #C5C5C5;
+  border: 1px solid rgba(0,0,0,0.08);
   font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
-  font-weight: 400;
+  font-weight: 200;
   font-size: 16px;
   height: 30px;
   width: 30px;
   margin-right: 5px;
   border-radius: 50%;
+  box-shadow: transparent 0px 0px 0px 1px, transparent 0px 0px 0px 4px, rgba(0, 0, 0, 0.18) 0px 2px 4px;
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.1);
+    transition-property: box-shadow, -ms-transform, -webkit-transform, transform !important;
+    transition-duration: 0.2s, 0.25s, 0.25s, 0.25s !important;
+    transition-timing-function: ease, ease, ease, ease !important;
+    transition-delay: 0s, 0s, 0s, 0s !important;
+  }
 `;
 
 const NextButton = styled.button`
-  border: 1px solid #C5C5C5;
+  border: 1px solid rgba(0,0,0,0.08);
   font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
-  font-weight: 400;
+  font-weight: 200;
   font-size: 16px;
   height: 30px;
   width: 30px;
   margin-left: 5px;
   border-radius: 50%;
+  box-shadow: transparent 0px 0px 0px 1px, transparent 0px 0px 0px 4px, rgba(0, 0, 0, 0.18) 0px 2px 4px;
+  cursor: pointer
+
+  &:hover {
+    transform: scale(1.1);
+    transition-property: box-shadow, -ms-transform, -webkit-transform, transform !important;
+    transition-duration: 0.2s, 0.25s, 0.25s, 0.25s !important;
+    transition-timing-function: ease, ease, ease, ease !important;
+    transition-delay: 0s, 0s, 0s, 0s !important;
+  }
 `;
 
 const Wrapper = styled.div`
