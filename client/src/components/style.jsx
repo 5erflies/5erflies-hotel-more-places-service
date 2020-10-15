d50b072fb08de08a08b648e376c4b551de6217ed
Tom Nguyen <tommy.nguyen0721@gmail.com>
2020-10-15 10:54:11 -0700

    Clean up code

--- client/src/components/style.jsx
+++ client/src/components/style.jsx
@@ -68,3 +68,81 @@ const NextButton = styled.button`
     transition-delay: 0s, 0s, 0s, 0s !important;
   }
 `;
+
+let starImg = "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Star-Image.png";
+let heartImg = "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/airbnb-heart.png"
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-width: 0px 10px;
+  flex: {
+    flex-grow: 0;
+    flex-shrink: 0;
+    flex-basis: 33.333%;
+  };
+  max-width: 33.333%;
+  font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
+  font-weight: 400;
+  font-size: 16px;
+  margin: 0 10px 0 10px;
+`;
+
+const ImageContainer = styled.div`
+  position: relative;
+`
+
+const Image = styled.img`
+  height: 100% || 120px;
+  width: 100%;
+  border-radius: 8px;
+  position: relative;
+  z-index: 0;
+`;
+
+const SuperHost = styled.div`
+  position: absolute;
+  font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
+  background-color: rgba(255, 255, 255, 0.95);
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+  margin: 10px;
+  color: rgb(34, 34, 34);
+  z-index: 1;
+`;
+
+const SuperHostCon = styled.span`
+  margin: 10px;
+  font-weight: bold;
+  font-size: 14px;
+`
+
+const HeartImg = styled.img`
+  height: 14px;
+  width: 16px;
+  position: absolute;
+  bottom: 88%;
+  left: 92%;
+  z-index: 1;
+`
+const StarImg = styled.img`
+  height: 14px;
+  width: 14px;
+  line-height: 0px;
+`;
+
+const NumRatings = styled.span`
+  color: grey
+`
+const Description = styled.div`
+  display: flex;
+  flex: {
+    flex-grow: 0;
+    flex-shrink: 0;
+    flex-basis: 33.333%;
+  };
+  max-width: 33.33%;
+`;
+const Price = styled.span`
+  font-weight: bold;
+`;
